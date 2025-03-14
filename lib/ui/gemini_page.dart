0f9095d4b4a998a0dcc9ca996bfbf3b60aa7935f
AqilFarros <aqilghonim@gmail.com>
2025-03-14 09:44:48 +0700

    first commit

---
+++ lib/ui/gemini_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class GeminiPage extends StatefulWidget {
+  const GeminiPage({super.key});
+
+  @override
+  State<GeminiPage> createState() => _GeminiPageState();
+}
+
+class _GeminiPageState extends State<GeminiPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Schedule Generator"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
